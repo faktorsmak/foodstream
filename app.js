19233ccc568f7e66d6a2cb10444bb33e5fbd300e
Josh <joshshemas@gmail.com>
2013-03-15 19:48:43 -0400

    remove uneed express stuff / build index page

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+	http = require('http'),
+	path = require('path'),
+	engine = require('ejs-locals'),
+	MemoryStore = require('connect').session.MemoryStore,
+	dbPath = 'mongodb://localhost/foodstream',
+	mongoose = require('mongoose');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 8080);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+// use ejs-locals for all ejs templates:
+app.engine('ejs', engine);
+
+// configure Express
+app.configure(function() {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.engine('ejs', engine);
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.session({ 
+		secret: 'eat more food', 
+		store: new MemoryStore() 
+	}));
+	mongoose.connect(dbPath, function onMongooseError(err) {
+		if (err) throw err;
+	});
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+// homepage
+app.get('/', function(req, res){
+	res.render('index',{pagename: 'Homepage'});
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+// error
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.send(500, 'Something broke!');
 });
+
+// run on port 8080
+app.listen(8080);
+console.log("Server Running!");
