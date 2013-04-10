1cfab4622ff3f4434a79d2340a3d693b6ae5000f
Josh Shemas <jshemas@Manta.com>
2013-04-10 19:14:03 -0400

    adding some validation functions to utils

--- routes/utils.js
+++ routes/utils.js
@@ -40,3 +40,66 @@ exports.resizeImage = function(inputPath, outputPath, width, height, callback) {
 		callback(err);
 	});
 }
+
+/**
+ * validate email
+ * @param string email - user input: email
+ * @param function callback
+ */
+exports.validateEmail = function(email, callback) {
+	// regex found here - http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+	var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return regex.test(email);
+};
+
+/**
+ * validate username
+ * @param string username - user input: username
+ * @param function callback
+ */
+exports.validateUsername = function(username, callback) {
+	//word characters such as 0-9, A-Z, a-z, _
+	//literal period
+	//literal @
+	//between 6 and 50 characters long
+	var regex = /^[\w\.@]{6,50}$/;
+	return regex.test(username);
+};
+
+/**
+ * validate password
+ * @param string password - user input: password
+ * @param function callback
+ */
+exports.validatePassword = function(password, callback) {
+	//word characters such as 0-9, A-Z, a-z, _
+	//literal period
+	//literal @
+	//between 6 and 50 characters long
+	var regex = /^[\w\.@]{6,50}$/;
+	return regex.test(password);
+};
+
+/**
+ * validate first name
+ * @param string first name - user input: first name
+ * @param function callback
+ */
+exports.validateFirstName = function(firstName, callback) {
+	//word characters such as A-Z and a-z
+	//between 2 and 30 characters long
+	var regex = /[a-zA-Z]{2,30}/;
+	return regex.test(firstName);
+};
+
+/**
+ * validate last name
+ * @param string last name - user input: last name
+ * @param function callback
+ */
+exports.validateLastName = function(lastName, callback) {
+	//word characters such as A-Z and a-z
+	//between 2 and 30 characters long
+	var regex = /[a-zA-Z]{2,30}/;
+	return regex.test(lastName);
+};
